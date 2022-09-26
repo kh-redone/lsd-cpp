577a870a601b2b8216a0ab33406f06188e5c50ac
kh-redone <90577405+kh-redone@users.noreply.github.com>
2022-09-26 22:16:31 +0100

    exo 3

---
+++ ses1/exo3_tp1.c
@@ -0,0 +1,13 @@
+#include "exo3_tp1.h"
+
+int main()
+{
+    int num;
+    printf("print a number : ");
+    scanf("%d",&num);
+    check_msb(num);
+    printf("\n");
+    print_bits(num);
+    return 0;
+
+}
